830f98b72b099033c2b5281031fb7f4b04159b38
seonysun <csh99240@naver.com>
2023-02-13 11:04:56 +0900

    Spring setting 0208

--- SpringDBTestProject/src/main/java/com/sist/dao/EmpDAO.java
+++ SpringDBTestProject/src/main/java/com/sist/dao/EmpDAO.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 import com.sist.mapper.*;
 @Repository //DAO 메모리할당
 public class EmpDAO {
-	@Autowired //Mapper(인터페이스) 메모리할당 -> 구현된 클래스값 받기
-	private EmpMapper mapper;
+	@Autowired //메모리할당 어노테이션
+	private EmpMapper mapper; //Mapper(인터페이스) 메모리할당 -> 구현된 클래스값 받기
 	
 	public List<EmpVO> empListData(){
 		return mapper.empListData();
