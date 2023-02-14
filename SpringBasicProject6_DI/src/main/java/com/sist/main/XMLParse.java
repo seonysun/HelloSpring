d315076c0f1b2c40b99fd85748ac494fa4cb05bc
seonysun <csh99240@naver.com>
2023-02-14 15:00:20 +0900

    spring 기본 0213 xml

--- SpringBasicProject6_DI/src/main/java/com/sist/main/XMLParse.java
+++ SpringBasicProject6_DI/src/main/java/com/sist/main/XMLParse.java
@@ -1,12 +1,29 @@
 package com.sist.main;
+import java.lang.reflect.Method;
 import java.util.*;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-public class XMLParse {
-	private Map map=new HashMap();
-	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException{
+public class XMLParse extends DefaultHandler{
+    private Map map=new HashMap();
+	public Map getMap() {
+		return map;
+	}
+	/*
+	 *   XML => JAXB / JAXP
+	 *                 ==== DOM/SAX(프레임워크) => Spring/MyBatis
+	 *                          ------------
+	 *                          <?XML version="1.0" encoding="UTF-8"> => startDocument() => 자동 
+	 *                          <beans> => startElement()
+	 *                            <bean id="" class=""/> => startElement() , endElement()
+	 *                            <bean id="" class=""/> => startElement() , endElement()
+	 *                          </beans> =>  endElement()
+	 *                          endDocument()
+	 */
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		try {
 			if(qName.equals("bean")) {
 				String id=attributes.getValue("id");
@@ -15,13 +32,21 @@ public class XMLParse {
 				String name=attributes.getValue("p:name");
 				String job=attributes.getValue("p:job");
 				String dept=attributes.getValue("p:dept");
+				
+				String s=attributes.getQName(3);
+				System.out.println(s);
 				Class clsName=Class.forName(cls);
 				Object obj=clsName.getDeclaredConstructor().newInstance();
+				System.out.println(attributes.getLength());
 				
 				Method[] methods=clsName.getDeclaredMethods();
 				for(Method m:methods) {
 					String mName=m.getName();
+					if(mName.equalsIgnoreCase("set"+s.substring(s.indexOf(":")+1))) {
+						m.invoke(obj, name);
+					}
 				}
+				map.put(id, obj);
 			}
 		} catch(Exception ex) {}
 	}
