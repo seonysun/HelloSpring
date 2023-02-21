9607cc56f84b59a7909845f933780057054ac274
seonysun <csh99240@naver.com>
2023-02-21 12:24:01 +0900

    spring 0221 mvc

--- SpringMVCProject1/src/main/java/com/sist/mapper/SeoulMapper.java
+++ SpringMVCProject1/src/main/java/com/sist/mapper/SeoulMapper.java
@@ -6,12 +6,12 @@ import org.apache.ibatis.annotations.Select;
 
 public interface SeoulMapper {
 	@Select("SELECT no,title,poster,num "
-			+ "FROM (SELECT no,title,poster,rownum as num "
-			+ "FROM (SELECT no,title,poster "
-			+ "FROM seoul_location ORDER BY no)) "
-			+ "WHERE num BETWEEN #{start} AND #{end}")
+			  +"FROM (SELECT no,title,poster,rownum as num "
+			  +"FROM (SELECT no,title,poster "
+			  +"FROM seoul_location ORDER BY no ASC)) "
+			  +"WHERE num BETWEEN #{start} AND #{end}")
 	public List<SeoulVO> seoulListData(Map map);
-	
+	   
 	@Select("SELECT CEIL(COUNT(*)/20.0) FROM seoul_location")
 	public int seoulTotalPage();
 }
