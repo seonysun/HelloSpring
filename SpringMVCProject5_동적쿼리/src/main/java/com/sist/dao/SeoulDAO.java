75b4be35fe8e246786d0cdd907691897ebe655a5
seonysun <csh99240@naver.com>
2023-02-26 22:56:27 +0900

    spring 0223 cookie

--- SpringMVCProject5_동적쿼리/src/main/java/com/sist/dao/SeoulDAO.java
+++ SpringMVCProject5_동적쿼리/src/main/java/com/sist/dao/SeoulDAO.java
@@ -11,26 +11,26 @@ import com.sist.mapper.*;
 public class SeoulDAO {
 	@Autowired
 	private SeoulMapper mapper;
+	//서울 목록
 	public List<SeoulVO> seoulListData(Map map){
 		return mapper.seoulListData(map);
 	}
-	
 	public int seoulTotalPage(Map map) {
 		return mapper.seoulTotalPage(map);
 	}
 	
+	//서울 상세
 	public SeoulVO seoulDetailData(Map map) {
 		return mapper.seoulDetailData(map);
 	}
-	
 	public List<FoodVO> foodListData(String addr){
 		return mapper.foodListData(addr);
 	}
 	
+	//서울 검색
 	public List<SeoulVO> seoulFindData(Map map){
 		return mapper.seoulFindData(map);
 	}
-	
 	public int seoulFindTotalPage(String addr) {
 		return mapper.seoulFindTotalPage(addr);
 	}
